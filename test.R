1aae36ffaeae7707f39826de5180c03218a47102
raf7000 <rafey.abbas@gmail.com>
2024-06-02 16:08:05 -0400

    save test

--- test.R
+++ test.R
@@ -1,19 +1,19 @@
 #monte carlo estimating e 
 
-e_estimation = function(num_of_trials) {
-  counts = numeric(num_of_trials)
-  for (i in 1:num_of_trials) {
-    sum = 0
-    count = 0 
+e_estimation = function(num_of_trials) { #defining function to estimate e 
+  counts = numeric(num_of_trials) #defining the number of counts as the number of trials 
+  for (i in 1:num_of_trials) { #setting a for loop 
+    sum = 0 #initializing sum 
+    count = 0 #initializing count 
     
-    while (sum <= 1) {
+    while (sum <= 1) { #if the sum <= 1, we will add a number between 0 and 1 to the sum 
       sum = sum + runif(1)
-      count <- count + 1
+      count <- count + 1 #incrementing count
     }
     
     counts[i] <- count
   }
-  mean(counts)  # The average count should converge to e
+  mean(counts)  #The average count should converge to e
 }
 
 
