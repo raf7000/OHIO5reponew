aabcbddbe593399900dc4cb405da712cc1e2d2af
raf7000 <132642236+raf7000@users.noreply.github.com>
2024-07-15 06:03:41 -0400

    Update fixredreduced2_mainscript.R

--- Fixed_Reduced2/fixredreduced2_mainscript.R
+++ Fixed_Reduced2/fixredreduced2_mainscript.R
@@ -13,6 +13,16 @@ subject_numbers <- c(10, 30, 50)
 results <- run_simulations_for_subject_numbers(subject_numbers)
 saveRDS(results, file = "simulation_results_list.rds")
 
+set.seed(123)  
+ni <- 30      
+m <- 30      
+beta <- c(1, 0, 1)  
+random_effects_var <- 1
+
+num_sims_list <- c(10, 100, 500)
+
+num_sims_results <- run_simulations_with_user_defined_sims(num_sims_list)
+saveRDS(num_sims_results, file = "num_sims_simulation_results_list.rds")
 
 
 
